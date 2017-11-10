6a6ad3a325ef67d0259f7122dfaee8583ed3bf05
akosoriod <akosoriod@unal.edu.co>
2017-11-10 13:36:04 -0500

    scale sendmail and notifications

--- sam_notifications_ms/app/models/notification.rb
+++
@@ -1,8 +0,0 @@
-class Notification < ApplicationRecord
-  validates :username, :sender, presence: true
-
-  def self.notifications(first_result = 0, max_result = 0)
-    max_result = Notification.count if max_result == 0
-    Notification.limit(max_result).offset(first_result)
-  end
-end
