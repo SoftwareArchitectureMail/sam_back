2580276d1a9b2dced7ac90f7c569893deb4ec411
David Barrera <davsanba@gmail.com>
2017-11-12 23:58:38 -0500

    Cambios en db

--- sam-sendmail-ms/start.sh
+++ sam-sendmail-ms/start.sh
@@ -1,5 +1,5 @@
 eval $(docker-machine env rancher-node2)
-docker-compose build 
-docker-compose run --rm sam_sendmail_ms rails db:create
-docker-compose run --rm sam_sendmail_ms rails db:migrate
+docker-compose build
+docker-compose run --rm sam-sendmail-ms rails db:create
+docker-compose run --rm sam-sendmail-ms rails db:migrate
 docker-compose up
